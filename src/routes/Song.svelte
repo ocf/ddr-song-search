c194ec915b7524204d600375929e379f2eb20e49
Roy Zhou <ren.zhou@berkeley.edu>
2022-10-23 14:42:19 -0700

    Make SongList

---
+++ src/routes/Song.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let difficulties;
+    let {title, subtitle, artist} = difficulties[0]
+    console.log(difficulties);
+</script>
+
+<div>
+{title} by {artist}
+</div>
+
+<style>
+    div {
+        background-color: aliceblue;
+        margin: 15px;
+	}
+</style>
---
+++ src/routes/SongList.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Song from './Song.svelte';
+    import allSongs from '../songs.json';
+	import Page from './+page.svelte';
+    let songKeys = Object.keys(allSongs);
+</script>
+
+<div>
+    {#each songKeys as songKey}
+    <Song difficulties={allSongs[songKey]} />
+    {/each}
+</div>
+
+
